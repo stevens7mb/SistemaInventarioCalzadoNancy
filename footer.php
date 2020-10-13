480e614d47cdcf35009b09da677ee623c4a9ac0c
Steven <steven10martin@gmail.com>
2020-10-13 09:32:46 -0600

    Commit de prueba

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="navbar navbar-default navbar-fixed-bottom">
     <div class="container">
-      <p class="navbar-text pull-left">&copy <?php echo date('Y');?> - Steven Martin B.
+      <p class="navbar-text pull-left">&copy <?php echo date('Y');?> - Steven Martin Bernardino.
            <a href="" target="_blank" style="color: #ecf0f1">Derechos Reservados</a>
       </p>
    </div>
