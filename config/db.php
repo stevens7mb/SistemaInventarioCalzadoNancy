8ce035d949c3cb68569c06869b7ac06b1571f3f7
Steven <steven10martin@gmail.com>
2020-10-13 10:36:38 -0600

    Cambio credenciales

--- config/db.php
+++ config/db.php
@@ -2,6 +2,6 @@
 /*Datos de conexion a la base de datos*/
 define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
 define('DB_USER', 'steven');//Usuario de tu base de datos
-define('DB_PASS', '140716');//Contraseña del usuario de la base de datos
+define('DB_PASS', '%gv6XXcS');//Contraseña del usuario de la base de datos
 define('DB_NAME', 'SistemaInventarioDB');//Nombre de la base de datos
 ?>
